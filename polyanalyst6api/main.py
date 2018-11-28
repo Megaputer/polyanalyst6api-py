0cb8f4044914c46746e0631c8858a1b1110e41b5
yatmanov <yatmanov@megaputer.ru>
2018-11-28 14:44:19 +0300

    Added type hints and fixed some type errors

--- polyanalyst6api/main.py
+++ polyanalyst6api/main.py
@@ -1,12 +1,10 @@
+from typing import Dict, List, Tuple, Union
 from urllib import parse
 
 import requests
-import urllib3
 
 from . import __version__
 from .exceptions import *
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 __all__ = ['ClientSession', 'Project', 'PAException', 'AuthException',
            'ClientException', 'APIException']
@@ -14,11 +12,16 @@ __all__ = ['ClientSession', 'Project', 'PAException', 'AuthException',
 API_PATH = '/polyanalyst/api/'
 API_VERSION = 'v1.0'
 
+# typing
+_Nodes = Dict[str, Dict[str, Union[int, str]]]
+_Response = Tuple[requests.Response, any]
+_Json = List[Dict[str, any]]
+
 
 class ClientSession:
     user_agent = f'PolyAnalyst6API python client v{__version__}'
 
-    def __enter__(self):
+    def __enter__(self) -> 'ClientSession':
         self.login()
         return self
 
@@ -28,26 +31,26 @@ class ClientSession:
         except NotImplementedError:
             return True
 
-    def __init__(self, netloc, username, password):
+    def __init__(self, netloc: str, username: str, password: str) -> None:
         self.netloc = netloc
         self.usr = username
         self.pwd = password
 
         self._s = requests.Session()
-        self.id = None  # session identity
+        self.id = ''  # session identity
 
         # path to certificate file. by default ignore insecure connection warnings
         self.certfile = False
 
-    def login(self):
+    def login(self) -> None:
         params = {'uname': self.usr, 'pwd': self.pwd}
         resp, _ = self.request('POST', 'login', params=params)
         self.id = resp.cookies['sid']
 
-    def logout(self):
+    def logout(self) -> None:
         raise NotImplementedError()
 
-    def project(self, uuid):
+    def project(self, uuid: str) -> 'Project':
         try:
             prj = Project(self, uuid)
             _ = prj.nodes
@@ -55,17 +58,18 @@ class ClientSession:
             raise
         return prj
 
-    def request(self, method, *paths, **requests_kwargs):
+    def request(self, method: str, *paths: str, **requests_kwargs) -> _Response:
         method = method.upper()
         url = self.construct_url(*paths)
+        requests_kwargs['verify'] = self.certfile
         try:
-            response = self._s.request(method, url, **requests_kwargs, verify=self.certfile)
+            response = self._s.request(method, url, **requests_kwargs)
         except requests.RequestException as e:
             raise ClientException(str(e))
 
         return self.handle_response(response)
 
-    def handle_response(self, response):
+    def handle_response(self, response: requests.Response) -> _Response:
         try:
             data = response.json()
         except ValueError:
@@ -91,7 +95,7 @@ class ClientSession:
         raise APIException(error_msg, response.url, response.status_code)
 
     @staticmethod
-    def get_error_details(data):
+    def get_error_details(data: Union[List[str], any]) -> str:
         if data and isinstance(data, list):
             msg = data[1]
         else:
@@ -99,7 +103,7 @@ class ClientSession:
 
         return msg
 
-    def construct_url(self, *paths):
+    def construct_url(self, *paths: str) -> str:
         url = parse.urljoin(self.netloc, API_PATH) + API_VERSION + '/'
         for path in paths:
             url = parse.urljoin(url, path)
@@ -107,14 +111,15 @@ class ClientSession:
 
 
 class Project:
-    def __init__(self, session, uuid):
+    def __init__(self, session: ClientSession, uuid: str) -> None:
         self.client_session = session
         self.id = uuid
 
-        self._nodes = {}
+        self._nodes: _Nodes = {}
 
     @property
-    def nodes(self):
+    def nodes(self) -> _Nodes:
+        json: Dict[str, _Json]
         _, json = self.client_session.request(
             'get',
             'project/', 'nodes',
@@ -127,21 +132,21 @@ class Project:
 
         return self._nodes
 
-    def save(self):
+    def save(self) -> None:
         _, _ = self.client_session.request(
             'post',
             'project/', 'save',
             json={'prjUUID': self.id}
         )
 
-    def abort(self):
+    def abort(self) -> None:
         _, _ = self.client_session.request(
             'post',
             'project/', 'global-abort',
             json={'prjUUID': self.id}
         )
 
-    def execute(self, node):
+    def execute(self, node: str) -> None:
         if node not in self.nodes:
             raise APIException(f"Node '{node}' is not found on the server")
         _, _ = self.client_session.request(
@@ -153,12 +158,13 @@ class Project:
             }
         )
 
-    def result(self, node):
+    def result(self, node: str) -> _Json:
         if node not in self.nodes:
             raise APIException(f"Node '{node}' is not found on the server")
-        _, data = self.client_session.request(
+        json: _Json
+        _, json = self.client_session.request(
             'get',
             'dataset/', 'preview',
             params={'prjUUID': self.id, 'name': node, 'type': self._nodes[node]['type']}
         )
-        return data
+        return json
