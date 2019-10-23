4e148d078cba354d4038d162575b0ff5934aa380
yatmanov <yatmanov@megaputer.ru>
2019-10-23 12:10:56 +0300

    Add a message attribute to APIException, remove _msg

--- polyanalyst6api/exceptions.py
+++ polyanalyst6api/exceptions.py
@@ -19,12 +19,12 @@ class APIException(PAException):
     """
 
     def __init__(self, msg: str, endpoint: str = None, status_code: int = None) -> None:
-        self._msg = msg
+        self.message = msg
         self.endpoint = endpoint
         self.status_code = status_code
 
     def __str__(self):
-        return f'{self._msg} ({self.status_code}, {self.endpoint})'
+        return f'{self.message} ({self.status_code}, {self.endpoint})'
 
 
 class ClientException(PAException):
