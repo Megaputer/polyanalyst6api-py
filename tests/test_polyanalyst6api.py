66c1c2d09fca901644552685926ff0fdc6ef8596
yatmanov <yatmanov@megaputer.ru>
2023-07-07 17:13:19 +0300

    Test package versioning

--- tests/test_polyanalyst6api.py
+++ tests/test_polyanalyst6api.py
@@ -4,6 +4,11 @@ import polyanalyst6api as pa
 import pytest
 
 
+def test_version():
+    mj, mn, pch = pa.__version__.split('.')
+    assert int(mj) == 0 and int(mn) >= 26 and pch != ''
+
+
 def test_api_version_arg_is_deprecated():
     with pytest.warns(DeprecationWarning):
         pa.API('https://localhost:5043', '', version=1)
